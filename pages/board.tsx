70d2a5e5577e85f9eea51c46e2c4bc5823d83d9b
Yoshiki Higuchi <s1f102201469@iniad.org>
2023-01-14 05:37:58 +0900

    feat: #1 初期配置機能の実装・セル表示機能の調整

--- pages/board.tsx
+++ pages/board.tsx
@@ -1,7 +1,30 @@
 import type { NextPage } from 'next'
-import React from 'react'
+import React, { useEffect } from 'react'
 import styled from 'styled-components'
-import { Cell } from 'pages/cells'
+import { Cell } from './cells'
+
+//ボム数、マスの状態を管理する
+export const boardState = {
+  None: 0,
+  Near1: 1,
+  Near2: 2,
+  Near3: 3,
+  Near4: 4,
+  Near5: 5,
+  Near6: 6,
+  Near7: 7,
+  Near8: 8,
+  Mine: 9,
+  Flag: 10,
+} as const
+export type BoardState = typeof boardState[keyof typeof boardState]
+
+//表示、非表示を管理する
+export const CellType = {
+  Close: 0,
+  Open: 1,
+} as const
+export type CellType = typeof CellType[keyof typeof CellType]
 
 const Container = styled.div`
   display: grid;
@@ -43,12 +66,78 @@ const InitialBoardData = [
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 ]
 
+//爆弾の数を数えた配列を返す関数
+const countBomb = (board: number[][]) => {
+  const countBoard = board.map((row: number[], i: number) => {
+    return row.map((cell: number, j: number) => {
+      //爆弾の数を数える
+      let count = 0
+      //もし爆弾があったらそのままの値にしておく
+      if (board[i][j] === boardState.Mine) {
+        return boardState.Mine
+      }
+      //上
+      if (board[i - 1] && board[i - 1][j] === boardState.Mine) {
+        count++
+      }
+      //下
+      if (board[i + 1] && board[i + 1][j] === boardState.Mine) {
+        count++
+      }
+      //左
+      if (board[i][j - 1] === boardState.Mine) {
+        count++
+      }
+      //右
+      if (board[i][j + 1] === boardState.Mine) {
+        count++
+      }
+      //左上
+      if (board[i - 1] && board[i - 1][j - 1] === boardState.Mine) {
+        count++
+      }
+      //右上
+      if (board[i - 1] && board[i - 1][j + 1] === boardState.Mine) {
+        count++
+      }
+      //左下
+      if (board[i + 1] && board[i + 1][j - 1] === boardState.Mine) {
+        count++
+      }
+      //右下
+      if (board[i + 1] && board[i + 1][j + 1] === boardState.Mine) {
+        count++
+      }
+      return count
+    })
+  })
+  return countBoard
+}
+
 //初期化関数
-
-
+const initBoard = (board: number[][]) => {
+  //爆弾の数
+  const bombCount = 8
+  //爆弾の配置
+  for (let i = 0; i < bombCount; i++) {
+    const x = Math.floor(Math.random() * 16)
+    const y = Math.floor(Math.random() * 16)
+    //CellのHasBombをtrueにする(配列上の値を9にする)
+    board[x][y] = boardState.Mine
+  }
+  return board
+}
 
 export const Board: NextPage = () => {
   const [board, setBoard] = React.useState(InitialBoardData)
+  useEffect(() => {
+    const Bombboard: number[][] = initBoard(board)
+    setBoard(Bombboard)
+    console.log(Bombboard)
+    //近くの爆弾の数を数える
+    console.log(countBomb(Bombboard))
+    setBoard(countBomb(Bombboard))
+  }, [])
   return (
     <Container>
       {board.map((row: number[], i: number) => {
@@ -56,10 +145,8 @@ export const Board: NextPage = () => {
           return (
             <Grid key={`${i}-${j}`}>
               <Cell
-                IsOpened={false}
-                HasBomb={false}
-                HasFlag={false}
-                NearBombCount={0}
+                Celltype={0}
+                Boardstate={board[i][j]}
               />
             </Grid>
           )
--- pages/cells.tsx
+++ pages/cells.tsx
@@ -1,122 +1,98 @@
 import React from 'react'
 import styled from 'styled-components'
-
-// export const CellsType = {
-//     None: 0,
-//     nearone: 1,
-//     neartwo: 2,
-//     nearthree: 3,
-//     bomb: 9
-// }
-// type CellsType = typeof CellsType[keyof typeof CellsType]
-
-
+import type { BoardState, CellType } from './board'
 
 // TODO 立体感を出す
-const CloseCell = styled.button`
+export const CloseCell = styled.button`
   width: 30px;
   height: 30px;
   background-color: silver;
   border: 1px solid black;
 `
 // TODO レイアウトを元ネタに寄せる
-const OpenCell = styled(CloseCell)`
+export const OpenCell = styled(CloseCell)`
   background-color: gray;
   border: 1px solid black;
 `
 
-const FlagCell = styled(CloseCell)`
+export const FlagCell = styled(CloseCell)`
   background: url(/minesweeper.png) no-repeat 69% 50%;
 `
 
-const BombCell = styled(CloseCell)`
+export const MineCell = styled(CloseCell)`
   background: url(/minesweeper.png) no-repeat 77% 50%;
 `
 
-const NearOneCell = styled(OpenCell)`
+export const NearOneCell = styled(OpenCell)`
   background: url(/minesweeper.png) no-repeat 0% 50%;
 `
 
-const NearTwoCell = styled(OpenCell)`
+export const NearTwoCell = styled(OpenCell)`
   background: url(/minesweeper.png) no-repeat 8% 50%;
 `
 
-const NearThreeCell = styled(OpenCell)`
+export const NearThreeCell = styled(OpenCell)`
   background: url(/minesweeper.png) no-repeat 16% 50%;
 `
 
-const NearFourCell = styled(OpenCell)`
+export const NearFourCell = styled(OpenCell)`
   background: url(/minesweeper.png) no-repeat 23% 50%;
 `
 
-const NearFiveCell = styled(OpenCell)`
+export const NearFiveCell = styled(OpenCell)`
   background: url(/minesweeper.png) no-repeat 31% 50%;
 `
 
-const NearSixCell = styled(OpenCell)`
+export const NearSixCell = styled(OpenCell)`
   background: url(/minesweeper.png) no-repeat 38.5% 50%;
 `
 
-const NearSevenCell = styled(OpenCell)`
+export const NearSevenCell = styled(OpenCell)`
   background: url(/minesweeper.png) no-repeat 46% 50%;
 `
 
-const NearEightCell = styled(OpenCell)`
+export const NearEightCell = styled(OpenCell)`
   background: url(/minesweeper.png) no-repeat 54% 50%;
 `
 
-const QuesttionCell = styled(OpenCell)`
+export const QuesttionCell = styled(OpenCell)`
   background: url(/minesweeper.png) no-repeat 62% 50%;
 `
 
 // セルの情報を定義
-interface Cellinfo {
-  IsOpened: boolean
-  HasBomb: boolean
-  HasFlag: boolean
-  NearBombCount: number
+export interface Cellinfo {
+  Celltype: number
+  Boardstate: number
 }
 
-const checkcelltype = (props: Cellinfo) => {
-  if (props.IsOpened === true) {
-    //開かれているかつ爆弾がある場合
-    if (props.HasBomb === true) {
-      return <BombCell />
-    }
-    //開かれているかつ爆弾がない場合
-    else {
-      switch (props.NearBombCount) {
-        case 1:
-          return <NearOneCell />
-        case 2:
-          return <NearTwoCell />
-        case 3:
-          return <NearThreeCell />
-        case 4:
-          return <NearFourCell />
-        case 5:
-          return <NearFiveCell />
-        case 6:
-          return <NearSixCell />
-        case 7:
-          return <NearSevenCell />
-        case 8:
-          return <NearEightCell />
-        default:
-          return <CloseCell />
-      }
-    }
-  } else {
-    //開かれていないかつ旗がある場合
-    if (props.HasFlag === true) {
+export const Cell = (props: Cellinfo) => {
+  //閉じているセルはCloseCellorFlagCellを表示
+  if (props.Celltype === 0) {
+    if (props.Boardstate === 10) {
       return <FlagCell />
     }
-    //開かれていないかつ旗がない場合
-    else {
-      return <CloseCell />
+  }
+  if (props.Celltype === 1) {
+    switch (props.Boardstate) {
+      case 0:
+        return <OpenCell />
+      case 1:
+        return <NearOneCell />
+      case 2:
+        return <NearTwoCell />
+      case 3:
+        return <NearThreeCell />
+      case 4:
+        return <NearFourCell />
+      case 5:
+        return <NearFiveCell />
+      case 6:
+        return <NearSixCell />
+      case 7:
+        return <NearSevenCell />
+      case 8:
+        return <NearEightCell />
     }
   }
-}
-export const Cell = (props: Cellinfo) => {
-  return checkcelltype(props)
+  return <CloseCell />
 }
